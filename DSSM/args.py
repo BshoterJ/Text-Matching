08257f19b5d4c0f2796877519881fc68914ff008
BshoterJ <Ginziwen@gmail.com>
2019-11-25 01:56:02 +0800

    Add DSSM model

---
+++ DSSM/args.py
@@ -0,0 +1,15 @@
+seq_length = 15
+
+char_embedding_size = 100
+
+learning_rate = 0.0005
+
+keep_prob = 0.8
+
+vocab_size = 7901
+
+class_size = 2
+
+epochs = 100
+
+batch_size = 1024
---
+++ DSSM/test.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+
+from Dssm.dssm import Graph
+import tensorflow as tf
+from utils.load_data import load_char_data
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+p, h, y = load_char_data('data/LCQMC_test.csv', data_size=None)
+
+model = Graph()
+saver = tf.train.Saver()
+
+with tf.Session()as sess:
+    sess.run(tf.global_variables_initializer())
+    saver.restore(sess, '../output/dssm/dssm_best.ckpt')
+    loss, acc = sess.run([model.loss, model.acc],
+                         feed_dict={model.p: p,
+                                    model.h: h,
+                                    model.y: y,
+                                    model.keep_prob: 1})
+
+    print('loss: ', loss, ' acc:', acc)
